c29111b12f99043c7d3005b510ec52a6f23522c3
BefeEzana <99324983+BefeEzana@users.noreply.github.com>
2022-03-17 03:45:57 +0300

    3tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_islower - entry point.
+ *@c: i dont know
+ * parameter i dont now
+ *Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c < 97)
+		return (0);
+	else
+		return (1);
+
+}

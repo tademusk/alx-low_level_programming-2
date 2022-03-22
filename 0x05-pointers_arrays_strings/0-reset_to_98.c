0906923d078578ac1f3af51ce285cbb657223af4
email <fekaduzana12@gmail.com>
2022-03-22 23:51:20 +0300

    the all files are essensial

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "main.h"
- /**
-  * reset_to_98 - function that takes a pointer to an int as parameter
-  * and updates the value it points to to 98.
-  *
-  * @n:number.
-  *Return:always 0.
-  */
- void reset_to_98(imt *n)
-{ *n = 98;}
+
+/**
+ * reset_to_98 - change the value of integer using pointer
+ * @n: pointer
+ * Return:void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

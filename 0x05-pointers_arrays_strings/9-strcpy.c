2e6e512e13c4e4074f82324e4319a12c009fe163
email <fekaduzana12@gmail.com>
2022-03-22 03:00:31 +0300

    91

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,22 @@
 #include "holberton.h"
-
 /**
- * reset_to_98 - Reset the variable sent to the function at 98
- * @n: Pointer to the integer sent to the function
+ * _strcpy - Copy a string
+ * @dest: Pointer to the string array for copying
+ * @src: Number of elements to extract
  *
  * Return: Nothing
 */
-
-void reset_to_98(int *n)
+char *_strcpy(char *dest, char *src)
 {
-	*n = 98;
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (dest);
 }

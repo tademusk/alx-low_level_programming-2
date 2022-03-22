58420ada0863a982e1e51a5773bb0559cf7fbfb1
email <fekaduzana12@gmail.com>
2022-03-22 03:18:28 +0300

    14

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - Reset the variable sent to the function at 98
- * @n: Pointer to the integer sent to the function
- *
- * Return: Nothing
-*/
-
+ * reset_to_98 - check the code for Holberton School students.
+ * @n : n is the pointer of n and return value 98
+ * Return: Always 0.
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;

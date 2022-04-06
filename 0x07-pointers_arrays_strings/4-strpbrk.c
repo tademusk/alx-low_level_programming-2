4491c893a0ea69255d6dc99334e00068fee05927
fekadu zana <fekaduzana12@gmail.com>
2022-04-07 00:35:56 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,22 +9,26 @@
   */
 char *_strpbrk(char *s, char *accept)
 {
-  int a = 0, b;
-  while (s[a])
-  {  
-    int a = 0, b;
 
-      while (accept[b])
-      {
-         if (s[a] == accept[b])
-           {
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
 				s += a;
 				return (s);
 			}
 
 			b++;
 		}
-        a++;
-  }
-  return ('\0');
+
+		a++;
+	}
+
+	return ('\0');
 }

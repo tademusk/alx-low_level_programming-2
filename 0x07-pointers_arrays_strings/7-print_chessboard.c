10b03ace47cbfcd03d6d85d4828e997008cd8b77
fekadu zana <fekaduzana12@gmail.com>
2022-04-07 01:03:50 +0300

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,18 +7,16 @@
   * Return: Nothing.
   */
 void print_chessboard(char (*a)[8])
-  {
+ {
 	int b, c;
 
-       for (b = 0; b < 8; b++)
+	for (b = 0; b < 8; b++)
 	{
 		for (c = 0; c < 8; c++)
 		{
 			_putchar(a[b][c]);
 		}
 
-         _putchar('\n');
-    
-    }
-
-  }      
+		_putchar('\n');
+	}
+}

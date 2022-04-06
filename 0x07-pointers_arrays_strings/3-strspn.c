bdde4bf2e565e09f8e45a8ec2bbcc9913253e5e9
fekadu zana <fekaduzana12@gmail.com>
2022-04-07 00:24:52 +0300

    strpn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,21 +9,24 @@
   */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int a= 0, b, t = 0;
+  unsigned int a = 0, b, t = 0;
 
-   while (accepy[a])
-   {
-       b = 0;
-       while (s[b] != 32)
-       {    
-           if (accept[a] == s[b])
-           {
-                t++;
-           }
-            b++;
-       }
+	while (accept[a])
+	{
+		b = 0;
 
-      a++;
-   }
-   return (t);
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (t);
 }

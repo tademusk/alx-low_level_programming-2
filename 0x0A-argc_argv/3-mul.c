d9ba6e6d788346e107ff710a487d2523802b9489
fekadu zana <fekaduzana12@gmail.com>
2022-04-06 17:59:30 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,15 +11,20 @@
  *         If the program does not receive two arguments - 1.
  */
 int main(int argc, char *argv[])
-  int num1, num2, prod;
-  if (argc != 3)
 {
-printf("Error\n");
-return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-prod = num1 * num2;
-printf("%d\n", prod);
-return(0);
+	int num1, num2, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
+
+	printf("%d\n", prod);
+
+	return (0);
 }

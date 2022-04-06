9c4cd1181935fa8477e892e6a45ac4d968398320
fekadu zana <fekaduzana12@gmail.com>
2022-04-07 00:50:26 +0300

    5T

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  *
+  * Return: char value
+  */
+char *_strstr(char *haystack, char *needle)
+{
+
+    int a = 0, b = 0;
+     
+     while (haystack[a])
+     {
+       while (need[b])
+       {
+              break;
+       }
+
+       b++;
+
+     }
+
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+	   }
+
+		a++;
+	}
+
+	return ('\0');
+}
+        
+

967cb2c6db4220fae61a53c51b0c21f88dc5fbf1
fekadu zana <fekaduzana12@gmail.com>
2022-04-06 17:55:18 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -10,6 +9,7 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-  printf("%s\n", argv[0];
-      return (0);
-      }
+   printf("%s\n", argv[0]);
+
+    return (0);
+}
